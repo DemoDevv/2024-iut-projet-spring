e0af3ee4416cf5f9c8c83cd01aa024378b030d3c
DemoDevv <mathieulebras95@gmail.com>
2025-01-21 20:50:11 +0100

    suppression de other config qui ne sert plus

--- products/src/main/kotlin/iut/nantes/project/products/configs/OtherConfig.kt
+++
@@ -1,8 +0,0 @@
-package iut.nantes.project.products.configs
-
-import org.springframework.context.annotation.Configuration
-import org.springframework.context.annotation.Profile
-
-//@Configuration
-//@Profile("!dev")
-//class OtherConfig
