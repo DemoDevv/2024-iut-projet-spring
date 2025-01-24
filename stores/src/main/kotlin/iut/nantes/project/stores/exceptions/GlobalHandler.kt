b011478bdbd17187cd98cd8e2be9b01872a6f762
DemoDevv <mathieulebras95@gmail.com>
2025-01-24 12:08:20 +0100

    ajout d'un handler pour les exceptions de base

--- stores/src/main/kotlin/iut/nantes/project/stores/exceptions/GlobalHandler.kt
+++ stores/src/main/kotlin/iut/nantes/project/stores/exceptions/GlobalHandler.kt
@@ -1,7 +1,15 @@
 package iut.nantes.project.stores.exceptions
 
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.ControllerAdvice
+import org.springframework.web.bind.annotation.ExceptionHandler
 
 @ControllerAdvice
 class GlobalHandler {
+    @ExceptionHandler(Exception::class)
+    fun handleGeneralException(ex: Exception): ResponseEntity<Map<String, String>> {
+        val body = mapOf("error" to "An unexpected error occurred", "details" to ex.message!!)
+        return ResponseEntity.status(500).body(body)
+    }
+
 }
