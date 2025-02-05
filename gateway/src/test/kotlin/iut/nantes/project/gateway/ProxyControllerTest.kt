5bc203c4ed373a2b8724f305927b8e1a310b17a3
slyberr <slyber.demi.sel@gmail.com>
2025-02-05 19:14:06 +0100

    mise à jour des tests proxy.

--- gateway/src/test/kotlin/iut/nantes/project/gateway/ProxyControllerTest.kt
+++ gateway/src/test/kotlin/iut/nantes/project/gateway/ProxyControllerTest.kt
@@ -1,16 +1,22 @@
 package iut.nantes.project.gateway
 
 import com.fasterxml.jackson.databind.ObjectMapper
+import com.jayway.jsonpath.JsonPath
+import org.apache.tomcat.util.codec.binary.Base64
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
 import org.springframework.context.annotation.Import
+import org.springframework.http.HttpStatus
 import org.springframework.http.MediaType
 import org.springframework.security.test.context.support.WithAnonymousUser
 import org.springframework.security.test.context.support.WithMockUser
+import org.springframework.test.web.reactive.server.JsonPathAssertions
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.get
 import org.springframework.test.web.servlet.post
+import org.springframework.test.web.servlet.put
+import kotlin.math.ceil
 
 @WebMvcTest
 @Import(TestConfiguration::class)
@@ -24,30 +30,22 @@ class ProxyControllerTest {
     @Autowired
     private lateinit var objectMapper: ObjectMapper
 
-    @WithAnonymousUser
-    @Test
-    fun `admin route without admin role`() {
-        mockMvc.get("/api/v1/families")
-            .andExpect {
-                status { isUnauthorized() }
-            }
+
+
+
+    private fun adminAutorisationForAnonymousTreatement():String{
+        val credentials = "ADMIN:ADMIN"
+        val encoded = Base64.encodeBase64String(credentials.toByteArray())
+        return "Basic $credentials"
     }
 
-    @WithMockUser(roles = ["ADMIN"])
-    @Test
-    fun `admin route with admin role`() {
-        mockMvc.get("/api/v1/families")
-            .andExpect {
-                status { isOk() }
-            }
-    }
 
     @WithMockUser(roles = ["ADMIN"])
     @Test
     fun `basic Admin GET request`() {
         mockMvc.get("/api/v1/products")
             .andExpect {
-                status { isOk() }
+                status { HttpStatus.OK }
             }
     }
     @WithAnonymousUser
@@ -55,7 +53,7 @@ class ProxyControllerTest {
     fun `basic anonymous GET request`() {
         mockMvc.get("/api/v1/products")
             .andExpect {
-                status { isUnauthorized() }
+                status { HttpStatus.UNAUTHORIZED}
             }
     }
 
@@ -65,7 +63,7 @@ class ProxyControllerTest {
 
         mockMvc.post("/api/v1/families", "{\"name\":\"Food\",\"description\":\"All foods\"}")
             .andExpect {
-                status { isUnauthorized() }
+                status { HttpStatus.UNAUTHORIZED }
             }
     }
 
@@ -80,12 +78,35 @@ class ProxyControllerTest {
             content = requestBody // Passe le JSON dans le body
         }
             .andExpect {
-                status { isCreated() }
+                status { HttpStatus.CREATED }
             }
-
-
-
     }
+
+
+    @WithMockUser(roles = ["ADMIN"])
+    @Test
+    fun `basic Admin PUT request`(){
+
+        val requestBody = """{"name":"Foodeeee","description":"All foods"}"""// Corps de la requête JSON
+
+        val request=mockMvc.post("/api/v1/families") {
+            contentType = MediaType.APPLICATION_JSON // Indique que c'est du JSON
+            content = requestBody // Passe le JSON dans le body
+        }.andReturn()
+
+        val response=request.response.contentAsString
+        val id : String = JsonPath.read(response,"$.id")
+
+        mockMvc.put("/api/v1/families/{id}",id) {
+            contentType = MediaType.APPLICATION_JSON // Indique que c'est du JSON
+            content = requestBody // Passe le JSON dans le body
+        } .andExpect {
+            status { HttpStatus.OK }
+        }
+    }
+
+
+
 
     @WithMockUser(roles = ["ADMIN"])
     @Test
