d5cde8ba225fa180e760b2dc38074d3c3b70ef7f
slyberr <slyber.demi.sel@gmail.com>
2025-02-08 16:07:13 +0100

    familly controller tests terminés.

--- products/src/test/kotlin/iut/nantes/project/products/controllerTests/FamilleControllerTest.kt
+++ products/src/test/kotlin/iut/nantes/project/products/controllerTests/FamilleControllerTest.kt
@@ -1,7 +1,10 @@
 package iut.nantes.project.products.controllerTests
 
 import iut.nantes.project.products.controllers.FamilleController
+import iut.nantes.project.products.controllers.ProductController
 import iut.nantes.project.products.controllers.dto.FamilleDto
+import iut.nantes.project.products.controllers.dto.Price
+import iut.nantes.project.products.controllers.dto.ProductDto
 import jakarta.validation.ConstraintViolationException
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
@@ -9,6 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.http.HttpStatus
+import java.util.*
 import kotlin.test.assertEquals
 
 @SpringBootTest
@@ -19,11 +23,8 @@ class FamilleControllerTest {
 
     @Autowired
     private lateinit var famillycontroller:FamilleController
-
-
-
-
-
+    @Autowired
+    private lateinit var productcontroller:ProductController
 
     @Test
     fun `basic case`() {
@@ -85,6 +86,129 @@ class FamilleControllerTest {
 
         famillycontroller.createFamily(famille)
         assertEquals(famillycontroller.createFamily(famille2).statusCode,HttpStatus.CONFLICT)
+
+    }
+
+    @Test
+    fun getAll() {
+        val famille = FamilleDto(id = null, name = "okay name", description = "this description")
+        val famille2 = FamilleDto(id = null, name = "very ok name", description = "a description")
+        val famille3 = FamilleDto(id = null, name = "very very ok name", description = "a description")
+
+        famillycontroller.createFamily(famille)
+        famillycontroller.createFamily(famille2)
+        famillycontroller.createFamily(famille3)
+        assertEquals(famillycontroller.getAllFamilies().body?.size ?: 0,3)
+
+    }
+
+    @Test
+    fun getbyId() {
+        val famille = FamilleDto(id = null, name = "okay name", description = "this description")
+
+        val body=famillycontroller.createFamily(famille).body as FamilleDto
+        val id = body.id
+
+        assertEquals(famillycontroller.getFamilyById(id ?: "").statusCode,HttpStatus.OK)
+
+    }
+
+    @Test
+    fun failGetbyId() {
+
+        val famille = FamilleDto(id = null, name = "okay name", description = "this description")
+        famillycontroller.createFamily(famille).body as FamilleDto
+        assertEquals(famillycontroller.getFamilyById(UUID.randomUUID().toString()).statusCode,HttpStatus.NOT_FOUND)
+
+    }
+
+    @Test
+    fun updateFamily() {
+        val famille = FamilleDto(id = null, name = "okay name", description = "this description")
+
+        val body=famillycontroller.createFamily(famille).body as FamilleDto
+        val id = body.id
+
+        val newFamille=FamilleDto("flk","New name","new description")
+
+        val result=famillycontroller.updateFamily(id ?:"",newFamille)
+        val body2=result.body as FamilleDto
+        val sameId=body2.id
+        val name=body2.name
+        val description=body2.description
+
+        assertEquals(listOf(name,description,sameId), listOf(newFamille.name,newFamille.description,id))
+
+    }
+
+    @Test
+    fun `Can't update family, not found`() {
+        val famille = FamilleDto(id = null, name = "okay name", description = "this description")
+
+        val body=famillycontroller.createFamily(famille).body as FamilleDto
+        val id = body.id
+        val newFamille=FamilleDto(null,"New name","new description")
+
+        val result=famillycontroller.updateFamily(UUID.randomUUID().toString(),newFamille)
+        val body2=result.body as FamilleDto
+        val sameId=body2.id
+        val name=body2.name
+        val description=body2.description
+
+        assertEquals(listOf(name,description,sameId), listOf(newFamille.name,newFamille.description,id))
+    }
+
+    @Test
+    fun `Can't update family, conflit`() {
+
+        val famille = FamilleDto(id = null, name = "okay name", description = "this description")
+        famillycontroller.createFamily(famille)
+
+        val familletoUpdate = FamilleDto(id = null, name = "AnotherName", description = "this description")
+        val body=famillycontroller.createFamily(familletoUpdate).body as FamilleDto
+        val id = body.id
+        val newFamille=FamilleDto(null,"okay name","new description")
+
+
+        assertEquals(famillycontroller.updateFamily(id ?: "",newFamille).statusCode,HttpStatus.CONFLICT)
+
+    }
+
+
+    @Test
+    fun deleteFamilly() {
+        val famille = FamilleDto(id = null, name = "okay name", description = "this description")
+
+        val body=famillycontroller.createFamily(famille).body as FamilleDto
+        val id = body.id
+
+        assertEquals(famillycontroller.deleteFamille(id ?: "").statusCode,HttpStatus.NO_CONTENT)
+
+    }
+
+    @Test
+    fun `Can't delete family, not found`() {
+        val famille = FamilleDto(id = null, name = "okay name", description = "this description")
+
+        famillycontroller.createFamily(famille).body as FamilleDto
+
+        assertEquals(famillycontroller.deleteFamille(UUID.randomUUID().toString()).statusCode,HttpStatus.NOT_FOUND)
+
+    }
+
+    @Test
+    fun `Can't delete family, conflict`() {
+
+        val famille = FamilleDto(id = null, name = "okay name", description = "this description")
+        val body=famillycontroller.createFamily(famille).body as FamilleDto
+        val id = body.id
+        famille.id=id
+
+        val produit=ProductDto(null,"a product","this is a product", Price(20,"EUR"),famille)
+        productcontroller.createProduct(produit)
+
+
+        assertEquals( famillycontroller.deleteFamille(id ?: "").statusCode,HttpStatus.CONFLICT)
 
     }
 
